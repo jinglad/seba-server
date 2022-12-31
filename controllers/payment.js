5120299a6ae31b66422b0394f98caa053f38335e
Jihan Chowdhury <jihanchowdhury70@gmail.com>
2022-12-31 19:15:11 +0600

    ipn redirect url added

--- controllers/payment.js
+++ controllers/payment.js
@@ -24,7 +24,7 @@ const initPayment = async (req, res) => {
     success: "yoursite.com/success", // If payment Succeed
     fail: "yoursite.com/fail", // If payment failed
     cancel: "yoursite.com/cancel", // If user cancel payment
-    ipn: "yoursite.com/ipn", // SSLCommerz will send http post request in this link
+    ipn: "https://seba-server.onrender.com", // SSLCommerz will send http post request in this link
   });
 
   payment.setOrderInfo({
